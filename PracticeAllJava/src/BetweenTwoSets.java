22a17ee7e9118bb1462f73849d0f7314440cc2d2
RaditzDev <superdockgenius@gmail.com>
2020-06-30 22:45:14 -0400

    Initial Code for two set problem Hackerank

---
+++ PracticeAllJava/src/BetweenTwoSets.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class BetweenTwoSets {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan  = new Scanner(System.in);
+		
+		String[] stringArr  = scan.nextLine().split(" ");
+		
+	    //String[] stringArr = SizeArr.split(" ");
+	    
+	    int[] firstArr = new int[Integer.parseInt(stringArr[0])];
+	    int [] secondArr =  new int[Integer.parseInt(stringArr[1])];
+	    
+	    
+	    String[] tempString = scan.nextLine().split(" ");
+	    
+	    for(int  i=0; i< firstArr.length;i++)
+	    	firstArr[i] = Integer.parseInt(tempString[i]);
+		
+		
+	tempString = scan.nextLine().split(" ");
+			
+	
+	 for(int  i=0; i< secondArr.length;i++)
+	    	secondArr[i] = Integer.parseInt(tempString[i]);
+	
+	 System.out.println("\n");
+	 
+	 System.out.println(Arrays.toString(firstArr));
+	 
+	 System.out.println("\n");
+	 
+	 System.out.println(Arrays.toString(secondArr));
+	 
+	 
+	}
+	
+	
+	
+	
+	
+}
